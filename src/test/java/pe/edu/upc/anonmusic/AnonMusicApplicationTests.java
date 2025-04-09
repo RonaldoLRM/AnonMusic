41dd4a53a55f9efdccc8d266fb334096f81d1612
RonaldoLRM <149207576+RonaldoLRM@users.noreply.github.com>
2025-04-09 14:39:01 -0500

    Add files via upload

---
+++ src/test/java/pe/edu/upc/anonmusic/AnonMusicApplicationTests.java
@@ -0,0 +1,13 @@
+package pe.edu.upc.anonmusic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnonMusicApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
