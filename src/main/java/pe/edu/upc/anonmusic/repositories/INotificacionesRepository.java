39f80b9ed4c34e0393e79ebe1e27d259d0532d29
06KJ <kim605j@gmail.com>
2025-06-10 12:27:11 -0500

    corrección de queries

--- src/main/java/pe/edu/upc/anonmusic/repositories/INotificacionesRepository.java
+++ src/main/java/pe/edu/upc/anonmusic/repositories/INotificacionesRepository.java
@@ -10,9 +10,9 @@ import java.util.List;
 @Repository
 public interface INotificacionesRepository extends JpaRepository<Notificaciones, Integer> {
     @Query(value = "SELECT n.tipo_notificacion, COUNT(*) AS cantidad_no_vistas\n" +
-            " FROM notificaciones n\n" +
-            " JOIN notificacionesxusuario npu ON n.id_notificacion = npu.id_notificacionesx_usuario\n" +
+            " FROM notificacionesxusuario npu\n" +
+            " JOIN notificaciones n ON npu.id_notificaciones = n.id_notificacion\n" +
             " WHERE npu.visto = FALSE\n" +
-            " GROUP BY n.tipo_notificacion;", nativeQuery = true)
+            " GROUP BY n.tipo_notificacion", nativeQuery = true)
     List<String[]> CantidadNotificacionesNoVistas();
 }
--- src/main/java/pe/edu/upc/anonmusic/repositories/IPublicacionesRepository.java
+++ src/main/java/pe/edu/upc/anonmusic/repositories/IPublicacionesRepository.java
@@ -11,10 +11,12 @@ import java.util.List;
 
 @Repository
 public interface IPublicacionesRepository extends JpaRepository<Publicaciones, Integer> {
-    @Query(value = "SELECT pu.id_publicacion, COUNT (co.id_comentario)\n" +
+    @Query(value = "SELECT pu.id_publicacion, COUNT (co.id_comentario) AS totalcomentario\n" +
             " FROM comentarios co\n" +
             " INNER JOIN publicaciones pu ON co.id_publicacion=pu.id_publicacion\n" +
-            " GROUP BY (pu.id_publicacion)", nativeQuery = true)
+            " GROUP BY (pu.id_publicacion)\n" +
+            " ORDER BY totalcomentario DESC \n" +
+            " LIMIT 5", nativeQuery = true)
     List<String[]> PublicacionesConMasComentarios();
 
     @Query(value = "SELECT fecha_publicacion, COUNT(*) AS cantidad_publicaciones\n" +
